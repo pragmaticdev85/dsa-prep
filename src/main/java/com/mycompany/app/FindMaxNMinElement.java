46edad9c7a1654b8c88e1e698ba901d756bd5dc5
Shanmukha K <shanmukha.k@gmail.com>
2024-07-09 09:47:30 +0400

    Add: Find max and min element in a given array

--- src/main/java/com/mycompany/app/FindMaxNMinElement.java
+++ src/main/java/com/mycompany/app/FindMaxNMinElement.java
@@ -1,20 +1,25 @@
 package com.mycompany.app;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
 import lombok.ToString;
 
 public class FindMaxNMinElement {
     public static MaxMinEl get(int[] arr) {
-
-        return null;
+        int minEl = Integer.MAX_VALUE;
+        int maxEl = Integer.MIN_VALUE;
+        for (int ix = 0; ix < arr.length; ix++) {
+            if (arr[ix] > maxEl) maxEl = arr[ix];
+            if (arr[ix] < minEl) minEl = arr[ix];
+        }
+        return MaxMinEl.builder().maxEl(maxEl).minEl(minEl).build();
     }
     public static void main(String[] args) {
         int[] a = {2, 1, -2, 4, 5, 0, 3};
         System.out.println(get(a));
     }
 }
-@Data
+@Builder
 @AllArgsConstructor
 @ToString
 class MaxMinEl{
