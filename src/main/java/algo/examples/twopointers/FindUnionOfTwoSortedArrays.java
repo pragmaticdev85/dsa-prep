ac929459ca9ab129cdec0255f6511d1386dd61d9
Shanmukha K <shanmukha.k@gmail.com>
2024-07-22 20:34:29 +0400

    Added FindUnionOfTwoSortedArrays demo

--- src/main/java/algo/examples/twopointers/FindUnionOfTwoSortedArrays.java
+++ src/main/java/algo/examples/twopointers/FindUnionOfTwoSortedArrays.java
@@ -1,6 +1,6 @@
 package algo.examples.twopointers;
 
-public class FindIntersectionOfTwoSortedArrays {
+public class FindUnionOfTwoSortedArrays {
     public static void main(String[] args) {
         int[] arrayOne = {1, 3, 4, 5, 7};
         int[] arrayTwo = {2, 3, 5, 7, 8};
@@ -12,9 +12,9 @@ public class FindIntersectionOfTwoSortedArrays {
                 System.out.print(arrayOne[arrOnePtr++] + " ");
                 ++arrTwoPtr;
             } else if (arrayOne[arrOnePtr] > arrayTwo[arrTwoPtr]) {
-                ++arrTwoPtr;
+                System.out.print(arrayTwo[arrTwoPtr++] + " ");
             } else if (arrayOne[arrOnePtr] < arrayTwo[arrTwoPtr]) {
-                ++arrOnePtr;
+                System.out.print(arrayOne[arrOnePtr++] + " ");
             }
         }
     }
