7fce1227985e6de3f63b0dd4224339b202d19a0a
Shanmukha K <shanmukha.k@gmail.com>
2024-09-23 21:50:53 +0530

    Added FibonacciDemo

---
+++ src/main/java/algo/examples/miscelaneous/FibonacciDemo.java
@@ -0,0 +1,23 @@
+package algo.examples.miscelaneous;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FibonacciDemo {
+    public static void main(String[] args) {
+        long st = System.currentTimeMillis();
+        int ix = 2;
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        int prevVal = list.get(0) + list.get(1);
+        while (ix < 30) {
+            prevVal = list.get(ix - 1) + list.get(ix - 2);
+            list.add(prevVal);
+            ++ix;
+        }
+        System.out.println(list);
+        System.out.printf("Time taken: %d ms%n", System.currentTimeMillis() - st);
+    }
+}
