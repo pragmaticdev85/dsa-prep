ce581b4d030c462bdb8fdfd9735f35a1eaa07f16
Shanmukha K <shanmukha.k@gmail.com>
2024-09-26 00:06:46 +0530

    Optimized Sort012 and re-formatted

--- src/main/java/algo/examples/sorting/Sort012Two.java
+++ src/main/java/algo/examples/sorting/Sort012Two.java
@@ -1,63 +1,48 @@
 package algo.examples.sorting;
+
 import java.util.*;
 import java.io.*;
-public class Sort012Two
-{
-    private static void sort(int[] arr)
-    {
+
+/**
+ * <b>Problem Statement:</b>
+ * <p>
+ * Given array of 0s, 1s and 2s randomly placed,
+ * <br/>implement an algorithm to sort them
+ */
+public class Sort012Two {
+    private static void sort(int[] arr) {
         int zerosCnt = 0;
         int onesCnt = 0;
         int twosCnt = 0;
-        for (int ix = 0; ix < arr.length; ix++)
-        {
-            switch(arr[ix])
-            {
-            case 0:
-                zerosCnt++;
-                break;
-            case 1:
-                onesCnt++;
-                break;
-            case 2:
-                twosCnt++;
-                break;
+        for (int ix = 0; ix < arr.length; ix++) {
+            switch (arr[ix]) {
+                case 0:
+                    zerosCnt++;
+                    break;
+                case 1:
+                    onesCnt++;
+                    break;
+                case 2:
+                    twosCnt++;
+                    break;
             }
         }
         int curIx = 0;
         for (int ix = 0; ix < zerosCnt; ix++) arr[curIx++] = 0;
         for (int ix = 0; ix < onesCnt; ix++) arr[curIx++] = 1;
         for (int ix = 0; ix < twosCnt; ix++) arr[curIx++] = 2;
-    }
-    public static void sort012(int[] args)
-    {
-        int cursor = 0;
-        int noOfInputs = args[cursor++];
-        List<int[]> outList = new ArrayList<>();
-        for (int ix = 0; ix < noOfInputs; ix++)
-        {
-            int arrayLength = args[cursor++];
-            int[] inputArr = new int[arrayLength];
-            for (int jx = 0; jx < arrayLength; jx++)
-            {
-                inputArr[jx] = args[cursor++];
-            }
-            sort(inputArr);
-            outList.add(inputArr);
-        }
         final StringBuilder sb = new StringBuilder();
-        for (int[] arr : outList)
-        {
-            sb.setLength(0);
-            sb.append(arr[0]);
-            for (int ix = 1; ix < arr.length; ix++)
-            {
-                sb.append(" ").append(arr[ix]);
-            }
-            System.out.println(sb.toString());
+        sb.setLength(0);
+        sb.append(arr[0]);
+        for (int ix = 1; ix < arr.length; ix++) {
+            sb.append(" ").append(arr[ix]);
         }
+        System.out.println(sb.toString());
     }
-    
+
     public static void main(String[] args) {
-    	sort012(new int[] {1, 2, 0, 1, 2, 0, 0, 1, 1, 1, 2, 2, 0});
-	}
+        long st = System.currentTimeMillis();
+        sort(new int[]{1, 2, 0, 1, 2, 0, 0, 1, 1, 1, 2, 2, 0});
+        System.out.printf("Elapsed Time: %d ms", (System.currentTimeMillis() - st));
+    }
 }
